cef1e4d33c2801ec4a2200e1d411d83400cdb38c
22070796 <22070796@vnu.edu.vn>
2025-06-07 01:52:23 +0700

    Code Cacche

---
+++ Ecommerce/sth/script.js
@@ -0,0 +1,7 @@
+// script.js
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
